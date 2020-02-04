19dac21a8402a971e8862181a344e74550b467b4
SagarJaybhay <jaybhay.sagar18@gmail.com>
2020-02-04 17:58:14 +0530

    Garbage collecion

--- Program.cs
+++ Program.cs
@@ -10,10 +10,28 @@ namespace Demos
     {
         static void Main(string[] args)
         {
-            //comments
-
-            //aaaaaa
-            //adada
+            demo d = new demo();
+            Console.WriteLine("Number of Generation Supporting "+ GC.MaxGeneration.ToString());
+            Console.WriteLine("Generation for this object  " + GC.GetGeneration(d));
+            Console.WriteLine("Generation Total Memory  " + GC.GetTotalMemory(true));
+            Console.Read();
         }
     }
+
+
+    class demo
+    {
+        private int i = 0;
+        private static string str = string.Empty;
+
+        public void calculate()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                str += i * 1000;
+            }
+        }
+    }
+
+
 }
